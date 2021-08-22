4d2b564b1f9c87a99aec642dbfa4132dea622058
zahike <zahike@yahoo.com>
2021-08-22 15:19:16 +0300

    Create clkDiv.v

---
+++ rtl/clkDiv.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.08.2021 19:24:29
+// Design Name: 
+// Module Name: clkDiv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clkDiv(
+input clk125,
+input rstn,
+output SerilsClk,
+output PixelClk
+    );
+assign SerilsClk = clk125;
+reg [2:0] Cnt_Div_Clk;
+always @(posedge clk125 or negedge rstn)
+    if (!rstn) Cnt_Div_Clk <= 3'b000;
+     else if (Cnt_Div_Clk == 3'b100) Cnt_Div_Clk <= 3'b000;
+     else Cnt_Div_Clk <= Cnt_Div_Clk + 1;
+reg Reg_Div_Clk;
+always @(posedge clk125 or negedge rstn)
+    if (!rstn) Reg_Div_Clk <= 1'b0;
+     else if (Cnt_Div_Clk == 3'b000)  Reg_Div_Clk <= 1'b1;
+     else if (Cnt_Div_Clk == 3'b010)  Reg_Div_Clk <= 1'b0;
+
+   BUFG BUFG_inst (
+      .O(PixelClk), // 1-bit output: Clock output
+      .I(Reg_Div_Clk)  // 1-bit input: Clock input
+   );
+    
+endmodule
